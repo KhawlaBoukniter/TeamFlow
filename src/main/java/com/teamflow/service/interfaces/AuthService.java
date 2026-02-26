67e55d05bd24bd2b3336fcd9843a896b45320399
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-02-26 11:02:42 +0000

    feat(auth): add changePassword method to AuthService interface

--- src/main/java/com/teamflow/service/interfaces/AuthService.java
+++ src/main/java/com/teamflow/service/interfaces/AuthService.java
@@ -4,6 +4,7 @@ import com.teamflow.dto.auth.AuthResponse;
 import com.teamflow.dto.auth.LoginRequest;
 import com.teamflow.dto.auth.RegisterRequest;
 import com.teamflow.dto.auth.TokenRefreshRequest;
+import com.teamflow.dto.auth.ChangePasswordRequest;
 
 public interface AuthService {
     AuthResponse register(RegisterRequest request);
@@ -13,4 +14,6 @@ public interface AuthService {
     AuthResponse refreshToken(TokenRefreshRequest request);
 
     void logout();
+
+    void changePassword(ChangePasswordRequest request);
 }
