e2f1d3408ccad46bb2481f2be0223325a7efc734
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-03-16 10:13:06 +0000

    feat: add searchTasks query to TaskRepository with access control

--- src/main/java/com/teamflow/repository/TaskRepository.java
+++ src/main/java/com/teamflow/repository/TaskRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +20,10 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     @Query("SELECT t FROM Task t JOIN t.assignments a WHERE a.user.id = :userId AND t.deletedAt IS NULL ORDER BY t.dueDate ASC")
     List<Task> findActiveTasksByUserId(
             @org.springframework.data.repository.query.Param("userId") Long userId);
+
+    @Query("SELECT DISTINCT t FROM Task t LEFT JOIN t.column c LEFT JOIN c.project p LEFT JOIN p.memberships m " +
+            "WHERE t.deletedAt IS NULL AND " +
+            "(t.title LIKE %:query% OR t.description LIKE %:query%) AND " +
+            "(p.owner.id = :userId OR (m.user.id = :userId AND m.deletedAt IS NULL))")
+    List<Task> searchTasks(@Param("query") String query, @Param("userId") Long userId);
 }
